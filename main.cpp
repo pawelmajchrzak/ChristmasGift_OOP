dbba6380dae1e361e754db8b1e9f43c8ef6b4bf8
Pawel Majchrzak <pawel.majchrzak.programista@gmail.com>
2022-10-10 23:13:30 +0200

    create Structur Person

--- main.cpp
+++ main.cpp
@@ -2,8 +2,35 @@
 
 using namespace std;
 
+struct Person
+{
+    int id = 0;
+    string name = "", email = "";
+};
+
+//string loadLine();
+//Uzytkownik pobierzDaneUzytkownika(string daneJednegoUzytkownikaOddzielonePionowymiKreskami);
+//void wczytajUzytkownikowZPliku(vector <Uzytkownik> &uzytkownicy);
+
+void implementationPerson (vector <Person> &persons)
+{
+
+    Person person = setDataNewPerson(persons);
+    persons.push_back(person);
+
+}
+
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    vector <Person> persons;
+    int numbersOfPersons=setNumbersOfPersons();
+    for (int i=0; i<numbersOfPersons; i++)
+    {
+        implementationPerson(persons);
+    }
+
+
     return 0;
 }
